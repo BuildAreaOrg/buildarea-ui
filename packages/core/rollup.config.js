0a3198cd1de2cfbb53c74000698180aff200114b
Samyak Bakliwal <samyak.b@mtechzilla.com>
2022-04-05 17:03:03 +0530

    format everything with prettier

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,27 +3,24 @@ import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "./src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-    },
-    {
-      file: pkg.module,
-      format: "es",
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    typescript({
-      clean: true,
-      tsconfig: "tsconfig-rollup.json",
-      typescript: require("typescript"),
-    }),
-    terser(), // always keep terser last
-  ],
+	input: "./src/index.ts",
+	output: [
+		{
+			file: pkg.main,
+			format: "cjs",
+		},
+		{
+			file: pkg.module,
+			format: "es",
+		},
+	],
+	external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+	plugins: [
+		typescript({
+			clean: true,
+			tsconfig: "tsconfig-rollup.json",
+			typescript: require("typescript"),
+		}),
+		terser(), // always keep terser last
+	],
 };
