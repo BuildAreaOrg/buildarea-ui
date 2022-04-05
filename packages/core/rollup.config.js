eee3b5747f93d2282fe905e15f0afb75f44be6f4
Samyak Bakliwal <samyak.b@mtechzilla.com>
2022-04-05 16:27:12 +0530

    refactor: change file structure for package

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,27 +3,27 @@ import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-	input: "./index.ts",
-	output: [
-		{
-			file: pkg.main,
-			format: "cjs",
-		},
-		{
-			file: pkg.module,
-			format: "es",
-		},
-	],
-	external: [
-		...Object.keys(pkg.dependencies || {}),
-		...Object.keys(pkg.peerDependencies || {}),
-	],
-	plugins: [
-		typescript({
-			clean: true,
-			tsconfig: "tsconfig-rollup.json",
-			typescript: require("typescript"),
-		}),
-		terser(), // always keep terser last
-	],
+  input: "./src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfig: "tsconfig-rollup.json",
+      typescript: require("typescript"),
+    }),
+    terser(), // always keep terser last
+  ],
 };
