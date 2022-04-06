1dd891f40a7c2894021c75367996a24e3780ae4b
Samyak Bakliwal <99634190+samyakb@users.noreply.github.com>
2022-04-06 19:13:55 +0530

    chore: add storybook (#3)

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+	stories: ["../packages/**/*.stories.tsx"],
+
+	// we need to add aliases to webpack so it knows how to follow
+	// to the source of the packages rather than the built version (dist)
+	webpackFinal: async (config) => ({
+		...config,
+		resolve: {
+			...config.resolve,
+			alias: {
+				...config.resolve.alias,
+				// ...convertTsConfigPathsToWebpackAliases(),
+			},
+		},
+	}),
+};
