01032dc7f9a0fec9daca444fd22bce1ad8ad4f58
Samyak Bakliwal <samyak.b@mtechzilla.com>
2022-04-22 18:52:07 +0530

    feat(Label): add new component

---
+++ src/Label/Label.stories.tsx
@@ -0,0 +1,15 @@
+import { Flex, Input } from "../index";
+import { Label } from "./Label";
+
+export const LabelDemo = () => (
+	<Flex align="center" css={{ padding: "0 20px", w: "$full" }}>
+		<Label htmlFor="companyName" css={{ lineHeight: "1rem", flexGrow: 2, width: "200px" }}>
+			Company Name
+		</Label>
+		<Input type="text" id="companyName" defaultValue="Buildarea" />
+	</Flex>
+);
+
+export default {
+	title: "Label",
+};
--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ export * from "./AlertDialog";
 export * from "./Checkbox";
 export * from "./Input";
 export * from "./Textarea";
+export * from "./Label";
