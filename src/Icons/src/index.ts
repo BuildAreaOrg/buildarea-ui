cb2a763f1ecced0ee871646a3d7a0c7c5171a859
Md Usman Ansari <106949096+MdUsmanAnsari06@users.noreply.github.com>
2022-08-30 11:39:54 +0530

    feat: add icons (#15)

---
+++ src/Icons/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./PhoneIcon";
+export * from "./EnvelopeIcon";
---
+++ src/Icons/src/types.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export interface IconProps extends React.SVGAttributes<SVGElement> {
+	children?: never;
+	color?: string;
+}
